3a116458b381a0e7edc2c2b4fa8f2e0cd925fc77
andomihaly <ando.mihaly@cleanit.hu>
2020-01-07 16:21:29 +0100

    add junit for intellij but not for maven

---
+++ src/test/java/JunitFrameworksSetUpTest.java
@@ -0,0 +1,16 @@
+package test.java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class JunitFrameworksSetUpTest {
+    @Test
+    public void TestSetUpIsOK(){
+        assertEquals(true, true);
+    }
+    @Test
+    public void TestSetUpIsNotOK(){
+        assertEquals(true, false);
+    }
+}
