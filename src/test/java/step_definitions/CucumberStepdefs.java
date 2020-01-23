39055d34a6ab8586e3406f34f0350a5b9bd6335d
andomihaly <ando.mihaly@cleanit.hu>
2020-01-23 10:52:00 +0100

    set up cucumber test runner via junit frameworks

--- src/test/java/step_definitions/CucumberStepdefs.java
+++ src/test/java/step_definitions/CucumberStepdefs.java
@@ -1,15 +1,18 @@
 package step_definitions;
 
+import io.cucumber.java.en.*;
+
 public class CucumberStepdefs {
-    @io.cucumber.java.en.Given("^intellij java project$")
+    @Given("intellij java project")
     public void intellijJavaProject() {
     }
 
-    @io.cucumber.java.en.When("^i start a new project$")
+    @When("i start a new project")
     public void iStartANewProject() {
     }
-
-    @io.cucumber.java.en.Then("^cucumber should work$")
+    @Then("cucumber should work")
     public void cucumberShouldWork() {
     }
+
+
 }
